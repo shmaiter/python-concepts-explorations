4380232e2e7cec408748ae22e9c9691cac005b0a
Shmaiter <shmaither@gmail.com>
2024-03-03 11:41:38 -0600

    (pdf_series): extracting and adding content to pdf files

--- t08-modify-pdf-files/01-extract-text.py
+++ t08-modify-pdf-files/01-extract-text.py
@@ -2,10 +2,10 @@ from PyPDF2 import PdfReader
 from pathlib import Path
 
 pdf_path = Path.cwd() / "Pride_and_Prejudice.pdf"
-pdf = PdfReader(str(pdf_path))
-print(f"Num pages: {len(pdf.pages)}")
-print(f"Doc Info: {pdf.metadata.title}")  # type: ignore
+pdf_reader = PdfReader(str(pdf_path))
+print(f"Num pages: {len(pdf_reader.pages)}")
+print(f"Doc Info: {pdf_reader.metadata.title}")  # type: ignore
 
-first_page = pdf.pages[9]
+first_page = pdf_reader.pages[9]
 text_page = first_page.extract_text()
 print(text_page)
--- t08-modify-pdf-files/03-pdf-to-pdf.py
+++ t08-modify-pdf-files/03-pdf-to-pdf.py
@@ -1,8 +1,40 @@
-from PyPDF2 import PdfWriter
+from PyPDF2 import PdfWriter, PdfReader
 from pathlib import Path
 
-pdf_writer = PdfWriter()
-pdf_writer.add_blank_page(width=72, height=72)
 
-with Path("blank.pdf").open(mode="wb") as output_file:
-    pdf_writer.write(output_file)
+# ******************************************* FIRST *******************************************
+# Adding a blank page
+# pdf_writer = PdfWriter()
+# pdf_writer.add_blank_page(width=72, height=72)
+
+# with Path("blank.pdf").open(mode="wb") as output_file:
+#     pdf_writer.write(output_file)
+
+# ******************************************* SECOND *******************************************
+# Taking a page from a pdf and adding it to a new empty one
+# pdf_path = Path.cwd() / "Pride_and_Prejudice.pdf"
+# pdf_reader = PdfReader(str(pdf_path))
+# first_page = pdf_reader.pages[0]
+# pdf_writer = PdfWriter()
+# pdf_writer.add_page(first_page)
+
+# new_pdf_path = Path.cwd() / "first_page.pdf"
+# with new_pdf_path.open(mode="wb") as output_file:
+#     pdf_writer.write(output_file)
+
+# ******************************************* THIRD *******************************************
+# Extracting multiple pages from a pdf and add them to a new empty one
+# pdf_path = Path.cwd() / "Pride_and_Prejudice.pdf"
+# pdf_reader = PdfReader(str(pdf_path))
+
+# pdf_writer = PdfWriter()
+# # for num_page in range(4):
+# #     page = pdf_reader.pages[num_page]
+# #     pdf_writer.add_page(page)
+
+# for page in pdf_reader.pages[1:4]:
+#     pdf_writer.add_page(page)
+
+# new_pdf_path = Path.cwd() / "first_chapter.pdf"
+# with new_pdf_path.open(mode="wb") as output_file:
+#     pdf_writer.write(output_file)
