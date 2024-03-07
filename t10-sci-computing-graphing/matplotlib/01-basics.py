98c669f9af43456e1371b873dc4c421023f0e430
Shmaiter <shmaither@gmail.com>
2024-03-07 11:57:42 -0600

    (matplotlib): fist commit on basic graphing

---
+++ t10-sci-computing-graphing/matplotlib/01-basics.py
@@ -0,0 +1,26 @@
+from matplotlib import pyplot as plt
+
+# xs = [1, 2, 3, 4, 5]
+# ys = [2, 4, 6, 8, 10]
+
+# plt.plot(xs, ys)
+# plt.show()
+
+# negative values
+# xs = [1, 2, 3, 4, 5]
+# ys = [3, -1, 4, 0, 6]
+# plt.plot(xs, ys)
+# plt.show()
+
+# green dots over line
+# plt.plot([2, 4, 6, 8, 10], "g-o")
+# plt.show()
+
+# Plot Multiple Graphs in the Same Window
+xs = [0, 1, 2, 3, 4]
+y1 = [1, 2, 3, 4, 5]
+y2 = [1, 2, 4, 8, 16]
+# plt.plot(xs, y1, xs, y2)
+# If you want to control the style of each graph
+plt.plot(xs, y1, "g-o", xs, y2, "b-^")
+plt.show()
